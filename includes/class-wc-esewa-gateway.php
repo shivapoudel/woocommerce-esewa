6a16d2b98b673e4a719f5f0239d8b132e4174754
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-04-29 03:40:26 +0545

    Workflow for basic settings

--- includes/class-wc-esewa-gateway.php
+++ includes/class-wc-esewa-gateway.php
@@ -20,14 +20,88 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class WC_Gateway_eSewa extends WC_Payment_Gateway {
 
+
+	/** @var boolean Whether or not logging is enabled */
+	public static $log_enabled = false;
+
+	/** @var WC_Logger Logger instance */
+	public static $log = false;
+
 	/**
 	 * Constructor for the gateway.
 	 */
 	public function __construct() {
 		$this->id                 = 'esewa';
-		$this->icon               = apply_filters( 'woocommerce_esewa_icon', '' );
-		$this->method_title       = __( 'eSewa', 'woocommerce-esewa' );
-		$this->method_description = __( 'eSewa works by sending customers to eSewa where they can enter their payment information.', 'woocommerce-esewa' );
+		$this->icon               = apply_filters( 'woocommerce_esewa_icon', plugins_url( 'assets/images/esewa.png', plugin_dir_path( __FILE__ ) ) );
 		$this->has_fields         = false;
+		$this->order_button_text  = __( 'Proceed to eSewa', 'woocommerce-esewa' );
+		$this->method_title       = __( 'eSewa', 'woocommerce-esewa' );
+		$this->method_description = __( 'The eSewa ePay system enables partner merchant to perform transaction and receive money from customer having eSewa account in secure environment.', 'woocommerce-esewa' );
+
+		// Load the settings.
+		$this->init_form_fields();
+		$this->init_settings();
+
+		// Define user set variables
+		$this->title          = $this->get_option( 'title' );
+		$this->description    = $this->get_option( 'description' );
+		$this->testmode       = 'yes' === $this->get_option( 'testmode', 'no' );
+		$this->debug          = 'yes' === $this->get_option( 'debug', 'no' );
+		$this->email          = $this->get_option( 'email' );
+		$this->receiver_email = $this->get_option( 'receiver_email', $this->email );
+		$this->identity_token = $this->get_option( 'identity_token' );
+
+		self::$log_enabled    = $this->debug;
+
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+
+		if ( ! $this->is_valid_for_use() ) {
+			$this->enabled = 'no';
+		}
+	}
+
+	/**
+	 * Logging method
+	 * @param string $message
+	 */
+	public static function log( $message ) {
+		if ( self::$log_enabled ) {
+			if ( empty( self::$log ) ) {
+				self::$log = new WC_Logger();
+			}
+			self::$log->add( 'esewa', $message );
+		}
+	}
+
+	/**
+	 * Check if this gateway is enabled and available in the user's country
+	 *
+	 * @return bool
+	 */
+	public function is_valid_for_use() {
+		return in_array( get_woocommerce_currency(), apply_filters( 'woocommerce_esewa_supported_currencies', array( 'NPR' ) ) );
+	}
+
+	/**
+	 * Admin Panel Options
+	 * - Options for bits like 'title' and availability on a country-by-country basis
+	 *
+	 * @since 1.0.0
+	 */
+	public function admin_options() {
+		if ( $this->is_valid_for_use() ) {
+			parent::admin_options();
+		} else {
+			?>
+			<div class="inline error"><p><strong><?php _e( 'Gateway Disabled', 'woocommerce-esewa' ); ?></strong>: <?php _e( 'eSewa does not support your store currency.', 'woocommerce-esewa' ); ?></p></div>
+			<?php
+		}
+	}
+
+	/**
+	 * Initialise Gateway Settings Form Fields
+	 */
+	public function init_form_fields() {
+		$this->form_fields = include( 'settings-esewa.php' );
 	}
 }
---
+++ includes/settings-esewa.php
@@ -0,0 +1,31 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Settings for eSewa Gateway
+ */
+return array(
+	'enabled' => array(
+		'title'   => __( 'Enable/Disable', 'woocommerce-esewa' ),
+		'type'    => 'checkbox',
+		'label'   => __( 'Enable eSewa Payment', 'woocommerce-esewa' ),
+		'default' => 'yes'
+	),
+	'title' => array(
+		'title'       => __( 'Title', 'woocommerce-esewa' ),
+		'type'        => 'text',
+		'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce-esewa' ),
+		'default'     => __( 'eSewa', 'woocommerce-esewa' ),
+		'desc_tip'    => true,
+	),
+	'description' => array(
+		'title'       => __( 'Description', 'woocommerce-esewa' ),
+		'type'        => 'text',
+		'desc_tip'    => true,
+		'description' => __( 'This controls the description which the user sees during checkout.', 'woocommerce-esewa' ),
+		'default'     => __( 'Pay via eSewa; you can pay with eSewa account securly.', 'woocommerce-esewa' )
+	)
+);
