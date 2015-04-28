5c8d22e7b706ca8e03293582dd795770c0b14a13
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-04-29 00:34:20 +0545

    Initial plugin file

---
+++ woocommerce-esewa.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WooCommerce eSewa
+ * Plugin URI: https://github.com/shivapoudel/woocommerce-esewa
+ * Description: WooCommerce eSewa is a Nepali payment gateway for WooCommerce.
+ * Author: AxisThemes
+ * Author URI: http://axisthemes.com
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: woocommerce-esewa
+ * Domain Path: /languages/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
