99131de6865129b060234b5d1d1d64c20f275424
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-04-29 08:58:46 +0545

    Added necessary setting fields

--- includes/settings-esewa.php
+++ includes/settings-esewa.php
@@ -28,12 +28,38 @@ return array(
 		'description' => __( 'This controls the description which the user sees during checkout.', 'woocommerce-esewa' ),
 		'default'     => __( 'Pay via eSewa; you can pay with eSewa account securly.', 'woocommerce-esewa' )
 	),
-	'merchant_id' => array(
+	'merchant' => array(
 		'title'       => __( 'Merchant ID', 'woocommerce-esewa' ),
 		'type'        => 'text',
 		'desc_tip'    => true,
-		'description' => __( 'Please enter eSewa Merchant ID; this is needed in order to take payment.', 'woocommerce-esewa' ),
+		'description' => __( 'Please enter your eSewa Merchant ID; this is needed in order to take payment.', 'woocommerce-esewa' ),
 		'default'     => '',
 		'placeholder' => 'Eg: 0000MID'
 	),
+	'paymentaction' => array(
+		'title'       => __( 'Payment Action', 'woocommerce-esewa' ),
+		'type'        => 'select',
+		'class'       => 'wc-enhanced-select',
+		'description' => __( 'Choose whether you wish to capture funds immediately or authorize payment only.', 'woocommerce-esewa' ),
+		'default'     => 'sale',
+		'desc_tip'    => true,
+		'options'     => array(
+			'sale'          => __( 'Capture', 'woocommerce-esewa' ),
+			'authorization' => __( 'Authorize', 'woocommerce-esewa' )
+		)
+	),
+	'testmode' => array(
+		'title'       => __( 'Test Mode', 'woocommerce-esewa' ),
+		'type'        => 'checkbox',
+		'label'       => __( 'Enable eSewa test mode', 'woocommerce-esewa' ),
+		'default'     => 'no',
+		'description' => __( 'Enable eSewa test mode to test payments.', 'woocommerce-esewa' ),
+	),
+	'debug' => array(
+		'title'       => __( 'Debug Log', 'woocommerce-esewa' ),
+		'type'        => 'checkbox',
+		'label'       => __( 'Enable logging', 'woocommerce-esewa' ),
+		'default'     => 'no',
+		'description' => sprintf( __( 'Log eSewa events, inside <code>%s</code>', 'woocommerce-esewa' ), wc_get_log_file_path( 'esewa' ) )
+	)
 );
