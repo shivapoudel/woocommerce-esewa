06e0a84904f4bf6a6846143b31d565e0aa857e6e
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-04-29 06:18:13 +0545

    Grunt Tasks registered

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+/* jshint node:true */
+module.exports = function( grunt ){
+	'use strict';
+
+	grunt.initConfig({
+
+		// Setting folder templates.
+		dirs: {
+			css: 'assets/css',
+			fonts: 'assets/fonts',
+			images: 'assets/images',
+			js: 'assets/js'
+		},
+
+		// JavaScript linting with JSHint.
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js'
+			]
+		},
+
+		// Generate POT files.
+		makepot: {
+			options: {
+				type: 'wp-plugin',
+				domainPath: 'languages',
+				potHeaders: {
+					'report-msgid-bugs-to': 'https://github.com/shivapoudel/woocommerce-esewa/issues',
+					'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
+				}
+			},
+			dist: {
+				options: {
+					potFilename: 'woocommerce-esewa.pot'
+				}
+			}
+		},
+
+		// Check textdomain errors.
+		checktextdomain: {
+			options:{
+				text_domain: 'woocommerce-esewa',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src: [
+					'**/*.php',
+					'!node_modules/**'
+				],
+				expand: true
+			}
+		}
+	});
+
+	// Load NPM tasks to be used here
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+
+	// Register tasks
+	grunt.registerTask( 'default', [
+		'makepot'
+	]);
+};
