44300015a7530aee39ffc62ce545878640892503
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-05-08 20:51:32 +0545

    eSewa Gateway response handler

---
+++ includes/includes/class-wc-gateway-esewa-handler.php
@@ -0,0 +1,14 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+include_once( 'class-wc-gateway-esewa-response.php' );
+
+/**
+ * Handles responses from eSewa
+ */
+class WC_Gateway_eSewa_Handler extends WC_Gateway_eSewa_Response {
+
+}
