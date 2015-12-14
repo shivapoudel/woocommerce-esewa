9e2eca837b50d7e8f35b71999fc7d9a55a2341dc
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-12-14 15:28:51 +0545

    Tweak the method description for gateway

--- includes/class-wc-esewa-gateway.php
+++ includes/class-wc-esewa-gateway.php
@@ -35,7 +35,7 @@ class WC_Gateway_eSewa extends WC_Payment_Gateway {
 		$this->has_fields         = false;
 		$this->order_button_text  = __( 'Proceed to eSewa', 'woocommerce-esewa' );
 		$this->method_title       = __( 'eSewa', 'woocommerce-esewa' );
-		$this->method_description = __( 'The eSewa epay system sends customers to eSewa to enter their payment information.', 'woocommerce-esewa' );
+		$this->method_description = sprintf( __( 'The eSewa epay system sends customers to eSewa to enter their payment information. The eSewa IPN requires fsockopen/cURL support to update order statuses after payment. Check the %ssystem status%s page for more details.', 'woocommerce-esewa' ), '<a href="' . admin_url( 'admin.php?page=wc-status' ) . '">', '</a>' );
 
 		// Load the settings.
 		$this->init_form_fields();
